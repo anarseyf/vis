c943d6ce00b71d8cc3902be6d7ab409fc3e1c7a9
Anar Seyf <anar.seyf@gmail.com>
2018-09-21 11:46:07 -0500

    App + router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlgorithmsComponent } from './algorithms/algorithms.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{
+    path: "algorithms",
+    component: AlgorithmsComponent
+}];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AlgorithmsComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes, {})
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
