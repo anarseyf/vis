63794e2bfb3359eea946f6385c05d2343645b17b
Anar Seyf <anar.seyf@gmail.com>
2018-09-21 12:47:27 -0500

    d3 circles

--- src/app/algorithms/algorithms.component.ts
+++ src/app/algorithms/algorithms.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
+import * as d3 from "d3";
 
 @Component({
     selector: 'app-algorithms',
     templateUrl: './algorithms.component.html',
     styleUrls: ['./algorithms.component.css']
 })
-export class AlgorithmsComponent implements OnInit {
 
-    constructor() { }
+export class AlgorithmsComponent implements OnInit, AfterContentInit {
+
+    data: object[];
+    count: number = 10;
+    width: number = 600;
+    height: number = 400;
+
+    constructor() {
+
+        this.data = new Array(this.count)
+            .fill(0)
+            .map(() => {
+                return {
+                    x: AlgorithmsComponent.randomCoordinate(this.width),
+                    y: AlgorithmsComponent.randomCoordinate(this.height) }
+            });
+
+        console.log(this.data);
+    }
+
+    static randomCoordinate(max: number) {
+        return Math.random() * max;
+    }
 
     ngOnInit() {
+    }
+
+    ngAfterContentInit() {
+        console.log(this.data);
+
+        let selection = d3.select("#svg")
+            .selectAll("circle")
+            .data(this.data);
+
+        selection.enter()
+            .append("circle")
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            })
+            .attr("r", 4)
+            .attr("fill", "darkslategray");
     }
 
 }
